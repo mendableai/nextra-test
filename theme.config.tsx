191a089f55cdba7529b8aba6f49e8846aa3629df
Nicolas <nicolascamara29@gmail.com>
2024-02-21 10:41:06 -0500

    Nick:

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { MendableSearchBar } from '@mendable/search'
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
@@ -13,6 +14,8 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Nextra Docs Template',
   },
+  search: {component:<div><MendableSearchBar anon_key="" showSimpleSearch /></div>},
+
 }
 
 export default config
